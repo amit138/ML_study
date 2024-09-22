ca64c5ed723889c9274a4aea3cc082563e316c0d
amit_ss_sharma <amit_ss_sharma@dell.com>
2024-09-22 22:23:08 +0530

    added code

---
+++ config.py
@@ -0,0 +1,43 @@
+
+
+#create a configurable file
+#%%writefile config.py
+
+class Config:
+  DEBUG = False
+  TESTING = False
+  DATABASE_URI = 'sqlite:///:memory:'
+  LOG_LEVEL = 'DEBUG'
+  LOG_FILE_PATH = 'app.log'
+  SECRET_KEY = 'mysecretkey'
+  SECRET_COOKIES = 'mysecretcookies'
+  API_ENDPOINT = 'https://api.example.com'
+  API_TIMEOUT = '10'
+  
+
+class ProductionConfig(Config):
+  DATABASE_URI = 'mysql://user@localhost/foo'
+  LOG_LEVEL = 'INFO'
+  LOG_FILE_PATH = 'app.log'
+  SECRET_KEY = 'mysecretkey-prod'
+  SECRET_COOKIES = 'mysecretcookies'
+  API_ENDPOINT = 'https://api.example.com'
+  API_TIMEOUT = '10'
+
+class DevelopmentConfig(Config):
+  DEBUG = True
+  LOG_LEVEL = 'INFO'
+  LOG_FILE_PATH = 'app.log'
+  SECRET_KEY = 'mysecretkey-dev'
+  SECRET_COOKIES = 'mysecretcookies'
+  API_ENDPOINT = 'https://api.example.com'
+  API_TIMEOUT = '20'
+
+class TestingConfig(Config):
+  TESTING = True
+  LOG_LEVEL = 'INFO'
+  LOG_FILE_PATH = 'app.log'
+  SECRET_KEY = 'mysecretkey-test'
+  SECRET_COOKIES = 'mysecretcookies'
+  API_ENDPOINT = 'https://api.example.com'
+  API_TIMEOUT = '30'
